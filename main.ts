2d5683fdec25fa8c6298636fb903008b6a2d2930
Horst Gutmann <zerok@zerokspot.com>
2023-08-02 22:08:51 +0200

    Add a very simple settings dialog

--- main.ts
+++ main.ts
@@ -1,81 +1,38 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+class DestinationSettings {
+	name?: string;
+	url?: string;
+	authHeaderName?: string;
+	authHeaderValue?: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface HTTPPublishPluginSettings {
+	destinations: DestinationSettings[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: HTTPPublishPluginSettings = {
+	destinations: []
+}
+
+export default class HTTPPublishPlugin extends Plugin {
+	settings: HTTPPublishPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'http-publish',
+			name: 'HTTP Publish',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new SettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,28 +48,14 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingTab extends PluginSettingTab {
+	plugin: HTTPPublishPlugin;
+	newDestination: DestinationSettings;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HTTPPublishPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
+		this.newDestination = {};
 	}
 
 	display(): void {
@@ -120,15 +63,74 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
+		new Setting(containerEl).setName("Destinations").setHeading();
+		if (this.plugin.settings.destinations.length <= 0) {
+			new Setting(containerEl).setName("No destinations set yet.")
+		} else {
+			for (const dest of this.plugin.settings.destinations) {
+				(destination => {
+					new Setting(containerEl)
+						.setName(destination.name || '')
+						.setDesc(destination.url || '')
+						.addButton(btn => {
+							btn.setButtonText("delete").onClick(async () => {
+								this.plugin.settings.destinations = this.plugin.settings.destinations.filter(d => {
+									return d.name !== destination.name;
+								});
+								await this.plugin.saveSettings();
+								this.display();
+							})
+						});
+				})(dest);
+			}
+		}
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Add new destination")
+			.setHeading()
+			.setDesc("A destination is a single HTTP endpoint that you want to send data to.");
+
+		new Setting(containerEl)
+			.setName("Name")
+			.addText(comp => {
+				comp.setPlaceholder("default").onChange((v) => {
+					this.newDestination.name = v;
+				}).setValue(this.newDestination.name || '');
+			});
+		new Setting(containerEl)
+			.setName("URL")
+			.addText(comp => {
+				comp.setPlaceholder("https://example.org").onChange((v) => {
+					this.newDestination.url = v;
+				}).setValue(this.newDestination.url || '');
+			});
+		new Setting(containerEl)
+			.setName("Authorization header name")
+			.addText(comp => {
+				comp.setPlaceholder("Authorization").onChange((v) => {
+					this.newDestination.authHeaderName = v;
+				}).setValue(this.newDestination.authHeaderName || '');
+			});
+		new Setting(containerEl)
+			.setName("Authorization header value")
+			.addText(comp => {
+				comp.setPlaceholder("Bearer 12345").onChange((v) => {
+					this.newDestination.authHeaderValue = v;
+				}).setValue(this.newDestination.authHeaderValue || '');
+			});
+		new Setting(containerEl).addButton(button => {
+			button.setButtonText("Add destination").onClick(async (evt) => {
+				// TODO: Validate those settings
+				this.plugin.settings.destinations.push({
+					name: this.newDestination.name,
+					url: this.newDestination.url,
+					authHeaderName: this.newDestination.authHeaderName,
+					authHeaderValue: this.newDestination.authHeaderValue
+				});
+				this.newDestination = {};
+				await this.plugin.saveSettings();
+				this.display();
+			});
+		})
 	}
 }
